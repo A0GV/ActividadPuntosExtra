046aaacd52ec817dd92bee4ba436b855bf707026
Adolfo <A01741767@tec.mx>
2025-10-15 13:38:24 -0600

    aa

---
+++ PuntosExtra/PuntosExtra/PuntosExtraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuntosExtraApp.swift
+//  PuntosExtra
+//
+//  Created by Alumno on 15/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PuntosExtraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
