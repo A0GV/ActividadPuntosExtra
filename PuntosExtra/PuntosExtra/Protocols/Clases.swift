d8a26f72f05b498490c2c2d1b721705dfa734b39
J-d3v <A00840297@tec.mx>
2025-10-15 13:57:22 -0600

    clase

---
+++ PuntosExtra/PuntosExtra/Protocols/Clases.swift
@@ -0,0 +1,10 @@
+//
+//  Clases.swift
+//  PuntosExtra
+//
+//  Created by Joel Tellez Hernández on 15/10/25.
+//
+
+import Foundation
+
+
--- PuntosExtra/PuntosExtra/Protocols/Comida.swift
+++ PuntosExtra/PuntosExtra/Protocols/Comida.swift
@@ -12,6 +12,8 @@ protocol ComidaProtocol{
     var Nombre: String { get set }
     var poder: Int { get set }
     var energia:Int { get set }
+    var sazon: Int { get set }
+    var grasa : Int { get set }
     
     func atacar() -> Int
     func recibirAtaque(valorAtaque: Int)
